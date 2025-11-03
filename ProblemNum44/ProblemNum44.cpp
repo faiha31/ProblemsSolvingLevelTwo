375a5cb483e06d32c764b04647a1ce0bbdee0570
faiha31 <alhadeedifaihaa@gmail.com>
2025-11-03 11:00:31 +0300

    add new problem solving

---
+++ ProblemNum44/ProblemNum44.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+using namespace std;
+
+/*Write a program to fill an array with max size 100 with random numbers from -100 to 100,
+then print the count of Positive numbers..*/
+
+
+int RandomNumber(int From, int To)
+{
+    int RandNum = rand() % (To - From + 1) + From;
+
+    return RandNum;
+}
+
+void FillArrayWithRandomNumber(int arr[100], int& NumberOfElemnts)
+{
+    cout << "Enter Number Of Element ?" << endl;
+    cin >> NumberOfElemnts;
+
+
+    for (int i = 0; i < NumberOfElemnts; i++)
+    {
+
+        arr[i] = RandomNumber(-100, 100);
+    }
+
+}
+
+void PrintArray(int arr[100], int NumberOfElemnts)
+{
+
+    for (int i = 0; i < NumberOfElemnts; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+}
+int CountPositiveNumbersInArrayElement(int arr[100], int& NumberOfElemnts)
+{
+    int Count = 0;
+
+    for (int i = 0; i < NumberOfElemnts; i++)
+    {
+        if (arr[i] >= 0)
+            Count++;
+    }
+    return Count;
+}
+
+
+
+int main()
+{
+    srand((unsigned)time(NULL));
+
+    int arr1[100], Arr1Length = 0;
+
+    FillArrayWithRandomNumber(arr1, Arr1Length);
+
+
+    cout << "\nArray1 Elements : \n";
+    PrintArray(arr1, Arr1Length);
+
+
+    cout << endl << "Array1 Positive Numbers Count is : "
+        << CountPositiveNumbersInArrayElement(arr1, Arr1Length) << endl;
+
+
+
+
+}
